29ebddd756e25957f1818f70796987e3fe6b103e
Abhishek Varshney <abhishekvarshney9336@gmail.com>
2026-09-05 09:05:58 +0530

    push

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ import { defineConfig, devices } from '@playwright/test';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: 'src/tests/',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
